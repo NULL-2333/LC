79f2713edfce86a8efc994c25f3e7f5a63210fab
NULL <450810699@qq.com>
2018-07-16 13:27:28 -0700

    1_TwoSum_Solution1
    最基本的暴力法，时间复杂度为O(n^2)

---
+++ 1_TwoSum_Solution1.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> ans;
+        int n = nums.size();
+        for(int i = 0; i < n; i++){
+            for(int j = i + 1;j < n; j++){
+                if(nums[i]+nums[j]==target){
+                    if(i<j) ans={i,j};
+                    else ans={j,i};
+                }
+            }
+        }
+        return ans;
+    }
+};
