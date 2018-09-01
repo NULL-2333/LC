2cb3644909ddc2edd12eea2f885920a7534970b8
NULL-2333 <450810699@qq.com>
2018-08-31 22:12:30 -0700

    Add new solution.

---
+++ 709_To Lower Case_Solution.cpp
@@ -0,0 +1,12 @@
+﻿class Solution {
+public:
+    string toLowerCase(string str) {
+        int a = str.length();
+        for(int i = 0;i < a;i++){
+            if(str[i] <= 'Z' && str[i] >= 'A'){
+                str[i] += 32;
+            }
+        }
+        return str;
+    }
+};
---
+++ 771_JewelsAndStones_Solution.cpp
@@ -0,0 +1,15 @@
+﻿class Solution {
+public:
+    int numJewelsInStones(string J, string S) {
+        int a = J.length(), b = S.length();
+	    int x = 0;
+	    for (int i = 0; i < a; i++)
+        {
+            for (int j = 0; j < b; j++)
+            {
+                if (J[i] == S[j]) x++;
+            }
+        }
+        return x;
+    }
+};
