2e58b960c6dedb79072f261c6f26c2426d8b5395
NULL-2333 <450810699@qq.com>
2018-09-03 11:51:17 -0700

    Add new solutions.

---
+++ 222_Count Complete Tree Nodes_Solution.cpp
@@ -0,0 +1,46 @@
+﻿/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int getLeftHeight(TreeNode* root){
+        int x = 0;
+        while(root != NULL){
+            x++;
+            root = root->left;
+        }
+        return x;
+    }
+    
+    int getRightHeight(TreeNode* root){
+        int x = 0;
+        while(root != NULL){
+            x++;
+            root = root->right;
+        }
+        return x;
+    }
+    
+    int countNodes(TreeNode* root) {
+        if(getLeftHeight(root) == getRightHeight(root))
+            return pow(2,getLeftHeight(root)) - 1;
+        else{
+            int xl = 0, xr = 0;
+            if(root != NULL){
+            if(root->left != NULL)
+                xl = countNodes(root->left);
+            if(root->right != NULL)
+                xr = countNodes(root->right);
+            }
+        
+            return 1 + xl + xr;
+        }
+        
+    }
+};
---
+++ 804_Unique Morse Code Words_Solution.cpp
@@ -0,0 +1,21 @@
+﻿class Solution {
+public:
+    int uniqueMorseRepresentations(vector<string>& words) {
+        int x = 0;
+        string m[] = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+        vector<string> mW(words.size());
+        for(int i = 0; i < words.size(); i++)
+            mW[i] = "";
+        for(int i = 0; i < words.size(); i++){
+            for(int j = 0; j < words[i].length(); j++){
+                mW[i] += m[words[i][j] - 'a'];
+            }
+        }
+        set<string> st;
+        for(int i = 0; i < words.size(); i++){
+            st.insert(mW[i]);
+        }
+        x = st.size();
+        return x;
+    }
+};
